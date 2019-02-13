3a468b3ee495f99301e05861fc20a497a781066c
Karishma Sawant <44118586+ksawant2@users.noreply.github.com>
2019-02-12 19:03:53 -0500

    Add files via upload

---
+++ Telco-Customer_R_code.R
@@ -0,0 +1,4 @@
+telco_Customer<- read.csv("C:/Users/sawan/OneDrive/Desktop/Backup KPS/Rutgers Courses/Sem 2/MA/project/Telco-Customer-Churn_Dataset.csv", stringsAsFactors=FALSE)
+View(telco_Customer)
+telco_Customer_df<-as.data.frame(telco_Customer)
+View(telco_Customer_df)
